135080042f240f86ef769f25faa81dba9113e101
Diego Pino Navarro <dpino@metro.org>
2023-07-25 15:48:50 -0400

    JS

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.archipelago_subtheme_rpi = {
+    attach: function (context, settings) {
+      if ($(context).is('.view') || context == document) {
+        /* Initialize Popovers */
+        var popoverTriggerList = [].slice.call(context.querySelectorAll('[data-bs-toggle="popover"]'))
+        var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+          return new bootstrap.Popover(popoverTriggerEl)
+        })
+        $("#main-breadcrumbs").find('.views-display-link').remove();
+        $(context).once('view-header').find('.view-header .views-display-link').each(function () {
+          $(this).detach().appendTo("#main-breadcrumbs");
+        });
+      }
+    }
+  }
+})(jQuery, Drupal);
